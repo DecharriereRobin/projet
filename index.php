d96cb7e0ed70759a6f41dfe80df5674cc1675819
Etudiant <Etudiant>
2016-10-28 11:01:35 +0200

    Ajout formulaire

--- index.php
+++ index.php
@@ -23,8 +23,15 @@
 		</nav>
 	</header><!-- /header -->
 
-
  <body>
- 	
+ 	<form action="" method="">
+		<label for="user">      <input type="text" name="user" id="user"  placeholder="user"/>          </label>
+		<label for="password"> <input type="password" name="password" id="password" placeholder="password"/></label>
+		<button type="submit" name="submit">Envoyer</button>
+		<button type="submit" name="create">Éditer</button>
+		<button type="submit" name="delete">Effacer</button>
+ 	</form>
  </body>
  </html>
+
+
